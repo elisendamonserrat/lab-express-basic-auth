5053b64d7d63f5809d579bc80e896b0f1590f041
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-05-04 15:14:57 +0200

    installs bcrypt npm package to hash the password from the form

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,25 @@
 const { Router } = require('express');
+const bcrypt = require('bcryptjs');
+
 const router = new Router();
+
+const saltRounds = 10;
 
 router.get('/sign-up', (req, res) => {
     res.render('auth/singup')
+});
+
+router.post('/sign-up', (req, res, next) => {
+    res.send(req.body)
+    const { username, password } = req.body;
+
+    bcrypt
+     .genSalt(saltRounds)
+     .then(salt => bcrypt.hash(password, salt))
+     .then((passwordHash) => {
+         console.log(passwordHash)
+     })
+     .catch(error => next(error))
 })
 
 
